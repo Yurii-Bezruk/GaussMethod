fc5cf764f2d2127de30f2d23ea13e6c123480614
Yurii Bezruk <yurii.bezruk.it@gmail.com>
2021-02-13 23:36:50 +0200

    Field repainting organized. Solving equation organaized. <ostly finished

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 
 public class Main {
-	public static void main(String[] args) {
-		/*java.util.Scanner scanner = new java.util.Scanner(System.in);
-		for(int i = 0; i < equation.length; i++) {
-			System.out.printf("Equation %d koefs:", i+1);
-			for(int j = 0; j < equation[i].length; j++) {
-				equation[i][j] = scanner.nextDouble();
-			} 			
-			
-		}*/
-		
+	public static void main(String[] args) {		
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -18,23 +9,6 @@ public class Main {
 					e.printStackTrace();
 				}
 			}
-		});
-		
-//		Matrix system = new Matrix(new Row[]{
-//			new Row(new double[]{1.21, 4.05, 2.11, 4.25}),
-//			new Row(new double[]{0.75, 1.21, 3.21, 7.42}),
-//			new Row(new double[]{2.27, 5.66, 3.06, 10.5})
-//		});		
-//		system.print();
-//		if(system.determinant() == 0) {
-//			System.err.println("Zero determinant! system cannot be solved.");
-//			return;
-//		}
-//		double[] roots = system.solve();
-//		system.print();	
-//		System.out.print("The roots is: (");
-//		for (double root : roots) 
-//			System.out.printf("%.2f; ", root);
-//		System.out.println(")");
+		});		
 	}
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,77 +1,58 @@
-
-
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
 
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	
+	private int dimention = 3;
 	private JPanel headPanel;	
 	private JLabel dimentionLabel;		
-	private JTextField dimentionTextField;
-	private int dimention = 3;
+	private JTextField dimentionTextField;	
+	private JLabel resultLabel;
+	private JTextField resultTextField;
 	
 	private JPanel centralPanel;
 	private JPanel[] matrixPanels;
 	private JTextField[][] matrixCoeffs;
-	private JLabel[][] matrixLabels;
+	private JLabel[][] matrixLabels;	
 	
 	private JPanel panelButtons;
 	private JButton solve;
 	private JButton exit;	
 	
 	public MainFrame() {
-		setResizable(false);
+		//setResizable(false);
 		setTitle("Linear System Gauss Method");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setBounds(150, 150, 400, 400);
+		setBounds(150, 150, 400, 400);		
 		contentPane = new JPanel();
 		contentPane.setLayout(new BorderLayout());
 		setContentPane(contentPane);
-		
-		panelButtons = new JPanel();
-		contentPane.add(panelButtons, BorderLayout.SOUTH);
-		
-		solve = new JButton("Solve");
-		solve.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		panelButtons.add(solve);
-		
-		exit = new JButton("Exit");
-		exit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		panelButtons.add(exit);
 				
 		headPanel = new JPanel();					
 		dimentionLabel = new JLabel("Dimention: ");
 		headPanel.add(dimentionLabel);
 		
 		dimentionTextField = new JTextField(String.valueOf(dimention));
-		dimentionTextField.setColumns(12);
-		dimentionTextField.addKeyListener(new EnterListener());
+		dimentionTextField.setColumns(2);
+		dimentionTextField.addActionListener(new DimentionListener());
 		headPanel.add(dimentionTextField);	
 		
+		resultLabel = new JLabel("Result: ");
+		headPanel.add(resultLabel);		
+		resultTextField = new JTextField("");
+		resultTextField.setColumns(20);
+		headPanel.add(resultTextField);
 		contentPane.add(headPanel, BorderLayout.NORTH);
 		
 		centralPanel = new JPanel(new FlowLayout());
@@ -90,19 +71,26 @@ public class MainFrame extends JFrame {
 		matrixCoeffs[2][0] = new JTextField("2.27");
 		matrixCoeffs[2][1] = new JTextField("5.66");
 		matrixCoeffs[2][2] = new JTextField("3.06");
-		matrixCoeffs[2][3] = new JTextField("10.5");
+		matrixCoeffs[2][3] = new JTextField("10.5");		
 		
-		for (int i = 0; i < dimention; i++) {
-			for (int j = 0; j < dimention + 1; j++) {
-				matrixPanels[i].add(matrixCoeffs[i][j]);
-				if(j < dimention)
-					matrixPanels[i].add(matrixLabels[i][j]);
-			}
-		}
-		for (JPanel panel : matrixPanels) 
-			centralPanel.add(panel);
+		establishMatrixForm();
 		
 		contentPane.add(centralPanel, BorderLayout.CENTER);
+		
+		panelButtons = new JPanel();
+		
+		solve = new JButton("Solve");
+		solve.addActionListener(new SolveButtonListener());
+		panelButtons.add(solve);
+		
+		exit = new JButton("Exit");
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		panelButtons.add(exit);
+		contentPane.add(panelButtons, BorderLayout.SOUTH);
 		
 		setVisible(true);
 	}
@@ -119,38 +107,65 @@ public class MainFrame extends JFrame {
 				String str = "x" + (j+1) + (j == dimention-1 ? " = " : " + ");
 				matrixLabels[i][j] = new JLabel(str);
 			}
-		}		
-
+		}
 	}
-	
-	private class EnterListener implements KeyListener {
+	private void establishMatrixForm() {
+		for (int i = 0; i < dimention; i++) {
+			for (int j = 0; j < dimention + 1; j++) {
+				if(matrixCoeffs[i][j] == null)
+					matrixCoeffs[i][j] = new JTextField("");
+				matrixCoeffs[i][j].setColumns(3);
+				matrixPanels[i].add(matrixCoeffs[i][j]);
+				if(j < dimention)
+					matrixPanels[i].add(matrixLabels[i][j]);
+			}
+		}
+		for (JPanel panel : matrixPanels) 
+			centralPanel.add(panel);
+	}
+	private class SolveButtonListener implements ActionListener{
 		@Override
-		public void keyTyped(KeyEvent e) {}
+		public void actionPerformed(ActionEvent e) {
+			Row[] rows = new Row[dimention];
+			double[] row = new double[dimention + 1];
+			for(int i = 0; i < dimention; i++) {
+				for (int j = 0; j < dimention + 1; j++) {
+					row[j] = Double.parseDouble(matrixCoeffs[i][j].getText());
+				}
+				rows[i] = new Row(Arrays.copyOf(row, row.length));
+			}			
+			Matrix system = new Matrix(rows);		
+			if(system.determinant() == 0) {
+				resultTextField.setText("Zero determinant! system cannot be solved.");
+			}
+			else {
+				double[] roots = system.solve();
+				resultTextField.setText("(");
+				for (double root : roots) 
+					resultTextField.setText(resultTextField.getText() + String.format("%.2f; ", root));
+				resultTextField.setText(resultTextField.getText() + ")");
+			}
+		}
+		
+	}
+	private class DimentionListener implements ActionListener {
 		@Override
-		public void keyPressed(KeyEvent e) {}
-		@Override
-		public void keyReleased(KeyEvent e) {
-//			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-//				if(parameterTextField.getText().equals(""))
-//					f = new AnalyticFunction(functionTextField.getText(), variableTextField.getText());
-//				else {
-//					f = new AnalyticFunction(functionTextField.getText(), variableTextField.getText(), parameterTextField.getText());
-//					if(!parameterValueTextField.getText().equals("")) {
-//						f.setParameterValue(Double.parseDouble(parameterValueTextField.getText()));
-//					}
-//				}
-//				start = Double.parseDouble(startTextField.getText());
-//				stop = Double.parseDouble(stopTextField.getText());
-//				step = Double.parseDouble(stepTextField.getText());
-//				
-//				function.clear();
-//				deritative.clear();
-//				for (double x = start; x < stop; x += step) {
-//					function.add(x, f.evalf(x));
-//					deritative.add(x, f.deritative().evalf(x));
-//				}
-//			}
-		}			
+		public void actionPerformed(ActionEvent e) {
+			for (int i = 0; i < dimention; i++) {
+				for (int j = 0; j < dimention + 1; j++) {
+					matrixPanels[i].remove(matrixCoeffs[i][j]);
+					if(j < dimention)
+						matrixPanels[i].remove(matrixLabels[i][j]);
+				}
+			}
+			for (JPanel panel : matrixPanels) 
+				centralPanel.remove(panel);
+			initializeMatrixForm();
+			establishMatrixForm();
+			
+			revalidate();
+			repaint();
+		}				
 	}
 
 }
